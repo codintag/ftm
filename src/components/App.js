f53c00c36a8e851a1a5f19cb4c3186a4a88fad3e
codintag <codintag@gmail.com>
2020-09-15 13:12:19 +0100

    project structure with packages installed

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './Home';
+import Navigbar from './Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
